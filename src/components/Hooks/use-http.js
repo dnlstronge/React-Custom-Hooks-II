05a510458d060d6f9a3018841d73264788ece559
Dan Stronge <dnlstronge@gmail.com>
2023-04-05 08:45:25 +0100

    [x] - create custom hook to handle fetch

---
+++ src/components/Hooks/use-http.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://connectdb-1efa3-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+}
+
+export default useHttp;
