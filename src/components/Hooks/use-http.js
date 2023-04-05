b786aab351b9b27f49ed0b98b2b7980a7121f888
Dan Stronge <dnlstronge@gmail.com>
2023-04-05 09:02:11 +0100

    [x] - refactor

--- src/components/Hooks/use-http.js
+++ src/components/Hooks/use-http.js
@@ -33,9 +33,9 @@ const useHttp = (requestConfig, applyData ) => {
   };
 
   return {
-    isLoading: isLoading,
-    error: error,
-    sendRequest: sendRequest
+    isLoading,
+    error,
+    sendRequest
   }
 }
 
